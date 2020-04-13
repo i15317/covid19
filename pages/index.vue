ec47d8aee42f974b46c32786e2ee93a5dac2e208
i15317 <30828280+i15317@users.noreply.github.com>
2020-04-13 14:12:29 +0900

    データを最新のものに更新

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       :url="
         'https://www.pref.kagawa.lg.jp/content/dir1/dir1_6/dir1_6_2/wt5q49200131182439.shtml#outbreak'
       "
-      :text="$t('香川県内での発症者数は1名です（2020/3/20 15:00時点）')"
+      :text="$t('香川県内での発症者数は8名です（2020/3/20 15:00時点）')"
     />
     <static-info
       class="mb-4"
